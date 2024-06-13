47b5e5dc28c0c2c3c1c351aba1bc27195f3b10c5
JOHN-AMAO <amaojohn767@gmail.com>
2024-06-13 21:54:12 +0100

    .

--- convex/auth.config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  providers: [
-    {
-      domain: "https://delicate-wasp-63.clerk.accounts.dev",
-      applicationID: "convex",
-    },
-  ],
-};
--- convex/schema.ts
+++
@@ -1,9 +0,0 @@
-import { defineSchema, defineTable } from "convex/server";
-import { v } from "convex/values";
-
-export default defineSchema({
-  Users: defineTable({
-    userId: v.string(),
-    userType: v.string(),
-  }),
-});
