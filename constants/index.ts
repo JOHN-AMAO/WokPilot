e241943f29275857b1e031503e20fef862cf7afd
JOHN-AMAO <amaojohn767@gmail.com>
2024-06-15 23:21:39 +0100

    constants

---
+++ constants/index.ts
@@ -0,0 +1,46 @@
+export const SidebarLinks = [
+  {
+    label: "Dashboard",
+    route: "/dashboard",
+    imgUrl: "/icons/Home.svg",
+  },
+  {
+    label: "Tansfer",
+    route: "/transfer",
+    imgUrl: "/icons/transfer.png",
+  },
+  {
+    label: "History",
+    route: "/history",
+    imgUrl: "/icons/history.png",
+  },
+  {
+    label: "Banks",
+    route: "/banks",
+    imgUrl: "/icons/banks.png",
+  },
+  {
+    label: "Bulk transfer",
+    route: "/bulk",
+    imgUrl: "/icons/add-personal.svg",
+  },
+];
+
+export const SidebarLinks2 = [
+  {
+    label: "Feature",
+    route: "/",
+  },
+  {
+    label: "Pricing",
+    route: "/",
+  },
+  {
+    label: "Security",
+    route: "/",
+  },
+  {
+    label: "contact",
+    route: "/",
+  },
+];
