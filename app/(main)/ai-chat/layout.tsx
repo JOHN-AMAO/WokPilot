e830cdc978e4fefd98c2c0891cc8d36985564665
JOHN-AMAO <amaojohn767@gmail.com>
2024-06-15 23:14:18 +0100

    app update

---
+++ app/(main)/ai-chat/layout.tsx
@@ -0,0 +1,19 @@
+import NavigationSidebar from "@/components/NavigationSidebar";
+import ProjectNavigation from "@/components/ProjectNavigation";
+import TaskNavigation from "@/components/TaskNavigation";
+import { SeparatorVerticalIcon } from "lucide-react";
+
+const MainLayout = async ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className='h-full dark:bg-black'>
+      <div className='hidden md:flex h-full  z-30 flex-row fixed inset-y-0'>
+        <ProjectNavigation />
+        <TaskNavigation />
+        <SeparatorVerticalIcon className='h-full bg-zinc-300 dark:bg-zinc-900 w-1' />
+      </div>
+      <main className='md:pl-[350px] h-full bg-black'>{children}</main>
+    </div>
+  );
+};
+
+export default MainLayout;
---
+++ app/(main)/ai-chat/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { AIchat } from "../../../components/component/aichat";
+
+const Page = () => {
+  return (
+    <div>
+      <AIchat />
+    </div>
+  );
+};
+
+export default Page;
